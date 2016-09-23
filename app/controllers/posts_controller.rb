8930acc6dd99add0ad5a0406f5d88f42b358e4fd
Matthew <matthew.giem@gmail.com>
2016-09-23 15:47:53 -0700

    create some of the basic pages for the posts model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save(post_params)
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:text)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
