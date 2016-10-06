8ef0cf4968aabbff42b33df6c0c36a92a0dc098e
Matthew <matthew.giem@gmail.com>
2016-10-06 16:41:25 -0700

    allow the user to go to the yahoo site to authenitcate then you can grab the auth token with the binding pry in omniauth_callback_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Yahoo") if is_navigational_format?
     else
-      session["devise.yahoo_data"] = request.env["omniauth.auth"]
+      session["devise.yahoo_data"] = request.env["omniauth.auth"][:credentials][:token]
+      binding.pry
       redirect_to new_user_registration_url
     end
   end
